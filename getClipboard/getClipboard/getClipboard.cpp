d4fba662f6e80ecdaa3ae338f3a432647aaa352e
pikovolt <kimagure@pikovolt.com>
2025-02-20 08:08:04 +0900

    initial commit

---
+++ getClipboard/getClipboard/getClipboard.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    HANDLE hData;
+    char* pszText;
+
+    // クリップボードを開く
+    if (!OpenClipboard(NULL)) {
+        std::cout << "クリップボードを開けませんでした。\n";
+        return 1;
+    }
+
+    // クリップボードからデータを取得
+    hData = GetClipboardData(CF_TEXT);
+    if (hData == NULL) {
+        CloseClipboard();
+        std::cout << "クリップボードからデータを取得できませんでした。\n";
+        return 1;
+    }
+
+    // データにロックをかける
+    pszText = static_cast<char*>(GlobalLock(hData));
+
+    // テキストを表示
+    std::cout << pszText << std::endl;
+
+    // ロックを解除
+    GlobalUnlock(hData);
+
+    // クリップボードを閉じる
+    CloseClipboard();
+
+    return 0;
+}
